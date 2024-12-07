e07cd7231835f351b1263e35127fed68a499b7d1
Yousef Eldosoky <eldosoky@yousefsite.com>
2024-12-07 14:50:57 +0200

    make the get endpoint

--- FilesEndpoint/server.js
+++ FilesEndpoint/server.js
@@ -1,15 +1,37 @@
-// server.mjs
+// import http
 const http = require('node:http');
 
-const server = http.createServer((req, res) => {
-    
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
+const fileSystem = require('./fileSystem'); // import fileSystem module
+
+
+const server = http.createServer(async (req, res) => {
+  if (req.method == "GET") {
+    //case empty url
+    if(req.url == '/') {
+        res.writeHead(403, { 'Content-Type': 'text/plain' });
+        res.end('Write the file path in ther url.\n');
+    } else {
+        try {
+            // get the last index of / to prevent the user from getting data outside this folder in the server.
+            lastIndex = req.url.split("").lastIndexOf("/")+1;
+
+            data = await fileSystem.readFileAsync(req.url.slice(lastIndex));
+
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({content: data}));
+        } catch (error) {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({error: error.message}));
+        }
+    }
+  } else {
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end("Method Not allowed!")
+  }
 });
 
-// starts a simple http server locally on port 3000
+// starts a server on port 3000
 server.listen(3000, '127.0.0.1', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
 
-// run with `node server.mjs`
