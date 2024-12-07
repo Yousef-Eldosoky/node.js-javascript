405c048a47c56d290682850a36195115edc87176
Yousef Eldosoky <eldosoky@yousefsite.com>
2024-12-07 16:13:49 +0200

    make the post endpoint

--- FilesEndpoint/server.js
+++ FilesEndpoint/server.js
@@ -2,14 +2,15 @@
 const http = require('node:http');
 
 const fileSystem = require('./fileSystem'); // import fileSystem module
+const { error } = require('node:console');
 
 
 const server = http.createServer(async (req, res) => {
   if (req.method == "GET") {
     //case empty url
     if(req.url == '/') {
-        res.writeHead(403, { 'Content-Type': 'text/plain' });
-        res.end('Write the file path in ther url.\n');
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Write the file name in ther url.\n');
     } else {
         try {
             // get the last index of / to prevent the user from getting data outside this folder in the server.
@@ -19,6 +20,47 @@ const server = http.createServer(async (req, res) => {
 
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({content: data}));
+        } catch (error) {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({error: error.message}));
+        }
+    }
+  } else if(req.method == "POST") {
+    //case empty url
+    if(req.url == '/') {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Write the file name in ther url.\n');
+    } else {
+        try {
+            // get the last index of / to prevent the user from getting data outside this folder in the server.
+            lastIndex = req.url.split("").lastIndexOf("/")+1;
+
+            //file extension validation
+            extension = req.url.slice(req.url.split("").lastIndexOf(".")+1);
+            if (extension != "txt" && extension != "text") {
+                res.writeHead(400, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({error: "file extension should be .txt or .text"}));
+                return;
+            }
+
+            let body = '';
+            req.on('data', chunk => {
+                body += chunk.toString(); // convert Buffer to string
+            });
+
+            req.on('end', async () => {
+                var bodyObj = JSON.parse(body);
+                body = bodyObj.content;
+                if (!body) { // check if the content in the content attribute
+                    res.writeHead(400, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({error: "Syntax error. Write the data in form of json file in content attribute."}));
+                    return;
+                }
+                data = await fileSystem.writeFileAsync(req.url.slice(lastIndex), body);
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({content: data}));
+            });
+
         } catch (error) {
             res.writeHead(404, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({error: error.message}));
